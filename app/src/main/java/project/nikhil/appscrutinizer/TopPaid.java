e8dc6f0c2f143e0dd1647bd9de8931474f435f2f
Nikhil Augustine <nikhilaug@gmail.com>
2015-04-06 11:26:55 +0530

    Initial commit

---
+++ app/src/main/java/project/nikhil/appscrutinizer/TopPaid.java
@@ -0,0 +1,18 @@
+package project.nikhil.appscrutinizer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * Created by nikhil on 2/4/15.
+ */
+public class TopPaid extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.toppaid_app,container,false);
+        return v;
+    }
+}
